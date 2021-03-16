6c7d95beb3b3f90571df2b1352c47878576ce785
Kim Wansu <wantalia@gmail.com>
2021-03-16 14:42:20 +0900

    학습, 검증 데이터 분리 테스트, 진행상황 표시

--- extract_summary.py
+++ extract_summary.py
@@ -1,12 +1,15 @@
 import os
 import json
 import time
+import random
 
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader
+
+from tqdm import tqdm
 
 from config import corpus_dir
 
@@ -57,7 +60,7 @@ def main():
 
     model_out = nn.Linear(2, 1).to(device)
 
-    lr = 0.1
+    lr = 0.01
     criterion = nn.BCELoss().to(device)
     optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=3)
@@ -66,27 +69,45 @@ def main():
     with open(corpus_dir + 'doc_summary4.json', 'r', encoding='utf-8') as f:
         j_doc = json.load(f)
 
+    # 문서 id 목록 로드
+    with open(corpus_dir + 'splitted_doclist.json', 'r', encoding='utf-8') as f:
+        j_list = json.load(f)
+
+    # 테스트용 문서 id
+    test_doc_ids = j_list['test']
+
+    # 검증용 문서 id
+    val_doc_ids = j_list['train'][0]
+
+    # 학습용 문서 id
+    train_doc_ids = []
+    for ids in j_list['train'][1:]:
+        train_doc_ids += ids
+
     # 입력 문서의 임베딩 로드
     embed_path = corpus_dir + 'summary_embed/'
 
     n_epochs = 1000
-    patience = 10  # 이 횟수만큼 성능 향상 없으면 학습 중단
+    max_patience = 10  # 이 횟수만큼 성능 향상 없으면 학습 중단
+    patience = 0
+
+    best_loss_val = float('inf')
+    best_epoch = 0
+    best_state = None
+
+    stop_by_val_data = False
 
     for epoch in range(n_epochs):
-        epoch_loss = 0.0
-        for i, fname in enumerate(os.listdir(embed_path)):
-            if '.embed.npy' not in fname:
-                continue
-
-            # TODO: cross-validation 넣기
-            # 위에서 doc_summary4.json 파일 열어서
-            # test에 속한 것인지, 몇번 train set에 속한 것인지 분류해놓기
-
-            # test, validate 제외하고 로드
-
-            doc_id = fname.replace('.embed.npy', '')
-
+        avg_loss_train = 0.0
+        avg_loss_val = 0.0
+        data_count = len(train_doc_ids)
+        random.shuffle(train_doc_ids)
+        for doc_id in tqdm(train_doc_ids):
+            fname = doc_id + '.embed.npy'
             full_fname = os.path.join(embed_path, fname)
+            if not os.path.exists(full_fname):
+                data_count -= 1
+                continue
 
             optimizer.zero_grad()
 
@@ -100,22 +121,55 @@ def main():
 
             label_Y = torch.Tensor(j_doc[doc_id]['labels'])
 
-            loss = criterion(label_H, label_Y)
-            epoch_loss += loss.item()
-
-            # 여기쯤에서 검증 말뭉치로 성능 측정 시도
-            # 일정 횟수 이상 성능 개선이 되지 않으면 마지막 최고성능 가중치로 복원 후 종료
-            # 중간중간 상태 기록 필요
-
-            loss.backward()
+            loss_train = criterion(label_H, label_Y)
+            loss_train.backward()
             optimizer.step()
 
-            val_loss = loss
-            scheduler.step(val_loss)
+            avg_loss_train += loss_train.item() / data_count
 
-        if epoch % 10 == 0:
-            print(f'== Epoch {epoch+1} ==')
-            print(f'loss: {epoch_loss}')
+        # 여기쯤에서 검증 말뭉치로 성능 측정 시도
+        # 일정 횟수 이상 성능 개선이 되지 않으면 마지막 최고성능 가중치로 복원 후 종료
+        # 중간중간 상태 기록 필요
+        with torch.no_grad():
+            v_data_count = len(val_doc_ids)
+            for v_doc_id in tqdm(val_doc_ids):
+                fname = v_doc_id + '.embed.npy'
+                v_full_fname = os.path.join(embed_path, fname)
+                if not os.path.exists(v_full_fname):
+                    v_data_count -= 1
+                    continue
+
+                v_embeds = np.load(v_full_fname)
+                v_embed_tensors = torch.Tensor(v_embeds).to(device)
+                v_output, v_status = model(v_embed_tensors)
+                v_label_H = torch.sigmoid(model_out(v_output))
+                v_label_H = v_label_H.squeeze(2)
+                v_label_H = v_label_H[:, -1].to(cpu)
+
+                v_label_Y = torch.Tensor(j_doc[v_doc_id]['labels'])
+
+                loss_val = criterion(v_label_H, v_label_Y)
+                avg_loss_val += loss_val.item() / v_data_count
+
+        if avg_loss_val < best_loss_val:
+            best_loss_val = avg_loss_val
+            best_epoch = epoch
+            best_state = model.state_dict()
+            patience = max_patience
+        else:
+            patience -= 1
+            stop_by_val_data = True
+            if patience == 0:
+                break
+
+        scheduler.step(avg_loss_val)
+
+        msg = 'NEW BEST' if patience == max_patience else f'patience={patience}'
+        print(f'[Epoch {epoch+1:>4}] train_cost = {avg_loss_train:>.9}, val_cost = {avg_loss_val:>.9} {msg} lr = {scheduler._last_lr}')
+
+    if stop_by_val_data:
+        print(f'Epoch {best_epoch:>4} stop by validation data.')
+        model.load_state_dict(best_state)
 
     model_state_fname = 'extract_summary.model'
     torch.save(model.state_dict(), model_state_fname)
